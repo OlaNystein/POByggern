23106124a380cb91e34e02f9ca352c0dc199c967
haavaul <haavaul@stud.ntnu.no>
2019-09-05 14:06:38 +0200

    latchen virker

--- main.c
+++ main.c
@@ -2,25 +2,27 @@
 #define F_CPU 4915200UL
 #include "util/delay.h"
 #include "uart.h"
+#include "SRAMtest.c"
 #define FOSC 4915200UL
 #define BAUD 9600
 #define MYUBURR FOSC/16/BAUD-1
 
 int main(void){
-    //UART_Init(MYUBURR);
-    //MCUCR |= (1<<SRE);
-    DDRE = 0;
-    DDRA = 0;
-    //DDRA = (0<<DDA0);
+    UART_Init(MYUBURR);
+    MCUCR |= (1<<SRE);
+    SFIOR|= (1<<XMM2);
+
+    //PORTA = 0x01;
+    volatile char *ext_ram = (char *) 0x1800;
+    ext_ram[16] = 1;
     while(1){
-        PORTA ^= (1<<PA0); //& (0<<PA1) & (0<<PA2) & (0<<PA3) & (0<<PA4) & (0<<PA5) & (0<<PA6) & (0<<PA7);     
-        PORTE = ~(1<<PE1);
-        _delay_ms(500);
-        PORTE = ~(0<<PE1);
-        _delay_ms(500);
-        //PORTE = (1<<PE1);
-        //printf(UART_Recieve());
-        //printf("Entered %c\r\n", UART_Recieve());
-}
+        //volatile char *ext_ram = (char *) 0x1800;
+        
+        //PORTA ^= (1<<PA0);   
+        //PORTE = ~(1<<PE1);
+        //_delay_ms(500);
+        //PORTE = ~(0<<PE1);
+        //_delay_ms(500);
+    }
     return 0;
 }
